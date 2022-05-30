55e9185403f2df984672568aa5821a0bbdc4f840
Rikard <rille_anderson@hotmail.com>
2022-05-30 14:44:19 +0200

    the code

--- AutoZip/Zipper.cs
+++ AutoZip/Zipper.cs
@@ -1,7 +1,20 @@
-﻿namespace AutoZip
+﻿using System;
+using System.IO.Compression;
+
+namespace AutoZip
 {
     public class Zipper
     {
+        static void Main(string[] args)
+        {
+            string startPath = @".\start";
+            string zipPath = @".\result.zip";
+            string extractPath = @".\extract";
+
+            ZipFile.CreateFromDirectory(startPath, zipPath);
+
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+        }
 
     }
 }
